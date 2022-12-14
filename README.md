546e991d8a862074f9229acc4820ffe888ee7c98
garima1520000 <105294195+garima1520000@users.noreply.github.com>
2022-12-14 14:33:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-6ef4pf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-6ef4pf)
---
+++ app.component.ts
@@ -0,0 +1,39 @@
+import { Component, ViewChild } from '@angular/core';
+import { recurrenceData } from './data';
+import { extend } from '@syncfusion/ej2-base';
+import {
+  ScheduleComponent, EventSettingsModel, EventRenderedArgs, View, DayService,
+  WeekService, MonthService, ResizeService, DragAndDropService
+} from '@syncfusion/ej2-angular-schedule';
+import { ChangeEventArgs } from '@syncfusion/ej2-buttons';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  providers: [DayService, WeekService, MonthService, ResizeService, DragAndDropService]
+})
+export class AppComponent {
+  public data: Record<string, any>[] = extend([], recurrenceData, null, true) as Record<string, any>[];
+  public selectedDate: Date = new Date(2021, 1, 20);
+  public eventSettings: EventSettingsModel = { dataSource: this.data };
+  public currentView: View = 'Week';
+  @ViewChild('scheduleObj') public scheduleObj: ScheduleComponent;
+
+  public onEventRendered(args: EventRenderedArgs): void {
+    const categoryColor: string = args.data.CategoryColor as string;
+    if (!args.element || !categoryColor) {
+      return;
+    }
+    if (this.currentView === 'Agenda') {
+      (args.element.firstChild as HTMLElement).style.borderLeftColor = categoryColor;
+    } else {
+      args.element.style.backgroundColor = categoryColor;
+    }
+  }
+
+  public onChange(args: ChangeEventArgs): void {
+    this.scheduleObj.eventSettings.editFollowingEvents = args.checked;
+  }
+
+}
